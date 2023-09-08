79337de258d570829ac3b46eaedcb86d431bf5dd
shooting_star <shooting_star>
2023-09-08 19:06:39 +0200

    Spelling

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 python -m venv ./venv
 ./venv/bin/pip install -r ./requirements.txt
-ECHO "Install Pyaudio via your prefered package manager"
+echo "Install Pyaudio via your prefered package manager"
